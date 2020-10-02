7be558074d3922ace2218604f04dc4c138a2d201
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2020-10-02 22:27:47 +0200

    completed the machine

--- javascript/box-machine/script.js
+++ javascript/box-machine/script.js
@@ -1,7 +1,7 @@
 //REMOVED JQUERY, WEIRD ISSUES?????
 const machine = document.getElementsByClassName('output');
 function makeShape() {
-    let circumference = document.getElementsByName('circum').value;
+    let circumference = document.getElementsByName('circum')[0].value; //value ger inte värdet?
     let shapeBtns = document.getElementsByName('shape');
     let colorBtns = document.getElementsByName('color');
     let shapeChoice = undefined;
@@ -18,10 +18,26 @@ function makeShape() {
             break;
         }
     }
-    //NÄSTA STEG - SKAPA NÅGOT FÖR FORMERNA??
-
-
     var newDiv = document.createElement('DIV');
+    if (shapeChoice.id == "triangle-button") {
+        newDiv.style.width = "0 px";
+        newDiv.style.height = "0 px";
+        newDiv.style.borderLeft = circumference / 2 + "px solid lime";
+        newDiv.style.borderRight = circumference / 2 + "px solid lime";
+        newDiv.style.borderBottom = circumference + "px solid " + colorChoice.value;
+    } else {
+        newDiv.style.width = circumference + "px";
+        newDiv.style.height = circumference + "px";
+        switch (shapeChoice.id) {
+            case "circle-button":
+                newDiv.style.borderRadius = "50%"
+                break;
+            case "cube-button":
+                break;
+            default:
+                break;
+        }
+    }
     newDiv.style.backgroundColor = colorChoice.value; //kanske måste översätta lådans värde till en faktiskt färg???? skapa variabler för varje färg som fylls när lådan fylls??
     machine[0].appendChild(newDiv);
 
